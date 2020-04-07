7f89057e5a7c789ef3fcac486e57601c45f5538f
urushibata <urush1bata.lala@gmail.com>
2020-04-07 21:48:55 +0900

    update

---
+++ official/officialContainerStart.sh
@@ -0,0 +1,8 @@
+# Mincraftのデータとホストを同期するディレクトリを作ります。
+
+sudo mkdir /var/mc_official_data/
+
+# Minecraftコンテナを起動します。
+
+cd $(cd $(dirname $0); pwd)
+docker-compose up -d
--- setupMinecraftServer.sh
+++ setupMinecraftServer.sh
@@ -23,22 +23,10 @@ sudo gpasswd -a $USER docker
 sudo chmod 766 /var/run/docker.sock
 
 # Docker-Composeをインストールします。
+
 COMPOSE_VERSION='1.25.4'
 sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod 755 /usr/local/bin/docker-compose
 
-# Mincraftのデータをホストに保存します。
-
-sudo mkdir /var/mc_official_data/
-
-# FWで25565番ポートを許可します。
-
-sudo iptables -A INPUT -p tcp --dport 25565 -j ACCEPT
-sudo /etc/init.d/netfilter-persistent save
-
-# Minecraftコンテナを起動します。
-
-cd $(cd $(dirname $0); pwd)
-docker-compose up -d
-
+source $(cd $(dirname $0); pwd)/official/officialContainerStart.sh
